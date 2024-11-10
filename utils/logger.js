1b5a8ab98fbf2e311eb53f7f6ba6002f83d8a1e8
ronnmabunga <ronnmabunga@gmail.com>
2024-11-10 21:30:25 +0800

    Add winston logger

---
+++ utils/logger.js
@@ -0,0 +1,12 @@
+const { createLogger, format, transports } = require("winston");
+
+const logger = createLogger({
+    level: "info",
+    format: format.combine(format.timestamp(), format.json()),
+    transports: [new transports.Console(), new transports.File({ filename: "combined.log" })],
+});
+
+module.exports = logger;
+module.exports.stream = {
+    write: (message) => logger.info(message.trim()),
+};
